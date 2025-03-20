2db50db7738ccfa01a0f006d2d4c9971cada35c2
Mark_Kamau <mark.wainaina@student.moringaschool.com>
2025-03-20 17:10:11 +0300

    completed looping code example

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// Function to return an array of thank-you messages
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+// Function to log numbers from the provided number down to 0
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+
